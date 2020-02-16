931fca0da8f8db45aa9a0a7910dd0bf6b2de0048
Andrea Lee <andrlee@cs.stonybrook.edu>
2020-02-16 13:58:02 -0500

    validargs returns flag for invalid and valid arguements

--- hw1/src/comdec.c
+++ hw1/src/comdec.c
@@ -32,6 +32,47 @@
  */
 
 /**
+ * Compares two strings to see if they are the same.
+ *
+ * @param as1 String to compare.
+ * @param as2 String to compare.
+ * @return -1 if as1 and as2 are different of 0 if as1 and as2 are the same.
+*/
+int string_compare(char *as1, char *as2) {
+    // same length
+    while (*as1 != '\0' || *as2 != '\0') {
+        if (*as1 == *as2) {
+            as1++;
+            as2++;
+        } else {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+/**
+ * Converts String of digits into an int
+ *
+ * @param as1 String to convert.
+ * @return int value of as1 or -1 if as1 was not made out of digits (0-9).
+*/
+int string_to_int(char *as1){
+    int value = 0;
+    for (int i=0; *(as1+i)!='\0'; i++) {
+        char digit = *(as1+i);
+        if (digit == '0' || digit == '1' || digit == '2' || digit == '3' || digit == '4'
+            || digit == '5' || digit == '6' || digit == '7' || digit == '8' || digit == '9') {
+            int num = digit - '0';
+            value = value * 10 + num;
+        } else {
+            return -1; // has to be digits only
+        }
+    }
+    return value;
+}
+
+/**
  * Main compression function.
  * Reads a sequence of bytes from a specified input stream, segments the
  * input data into blocks of a specified maximum number of bytes,
@@ -89,8 +130,54 @@ int decompress(FILE *in, FILE *out) {
  * @modifies global variable "global_options" to contain a bitmap representing
  * the selected options.
  */
-int validargs(int argc, char **argv)
-{
-    // To be implemented.
+int validargs(int argc, char **argv) {
+    // no flags = invalid
+    if (argc==1) {
+        global_options = 0;
+        return -1;
+    }
+
+    //*(argv+0) = "bin/sequitur"
+    // First Flag
+    char *a1 = *(argv+1);
+    // -h
+    if(string_compare(a1,"-h")==0) {
+        // -h : least significant bit (bit0) = 1
+        global_options = 1; ////////////////////////////////////////
+        // ignore rest
+        return 0; // success if has -h flag
+    }
+    // -c
+    if (string_compare(a1,"-c")==0) {
+        // just -c tag
+        if(argc==2) {
+            // -c : second-least-significant bit (bit1) = 1
+            global_options = 1; ///////////////////////////////////////////////////
+            //16 most-significant bits = 0x0400
+            global_options = 1; ///////////////////////////////////////////////////
+            return 0;
+        }
+        // -b tag with BLOCKSIZE
+        if(argc==4) {
+            char *a2 = *(argv+2);
+            char *a3 = *(argv+3); // BLOCKSIZE must be digits '0-9' and range 1-1024
+            int bs = string_to_int(a3);
+            if(string_compare(a2,"-b")==0 && bs!=-1) {
+                // -c : second-least-significant bit (bit1) = 1
+                global_options = 1; ///////////////////////////////////////////////////
+                // BLOCKSIZE = 16 most-significant bits
+                global_options = bs; ///////////////////////////////////////////////////
+                return 0;
+            }
+        } // too many/few arguments
+    }
+    // -d && no more arguements afterwards
+    if (string_compare(a1,"-d")==0 && argc==2) {
+        // -d : third-least-significant bit (bit2) = 1
+        global_options = 1; ////////////////////////////////////////////////
+        return 0;
+    }
+    // not -h|-c|-d
+    global_options = 0;
     return -1;
 }
