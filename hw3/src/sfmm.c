8a14e4e5c024fc62ab40d1e456fc3ec75378758c
Andrea Lee <andrlee@cs.stonybrook.edu>
2020-03-22 01:05:29 -0400

    initialize heap in malloc

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -9,7 +9,51 @@
 #include "debug.h"
 #include "sfmm.h"
 
+#define M 64 // minimum size in bytes
+
 void *sf_malloc(size_t size) {
+    if(size==0) {
+        return NULL;
+    }
+
+    // else size !=0
+
+    // if heap is empty
+    if(sf_mem_start()==sf_mem_end()) { // initial call
+        sf_mem_grow();
+        // create prologue
+        sf_block *bp = (sf_block *)(sf_mem_start()+(M-(sizeof(sf_header)+sizeof(sf_footer))));
+        //48
+        // header = [al: 1, sz:      64, pal: 1][PROLOGUE]
+        bp->header = M & BLOCK_SIZE_MASK; // set block_size to 64
+        bp->header |= THIS_BLOCK_ALLOCATED; // alloc = 1
+        bp->header |= PREV_BLOCK_ALLOCATED; //prev_alloc = 1
+
+        // next block in the heap = free/alloc blocks
+        sf_block *nbp = (sf_block *)(sf_mem_start()+((M-(sizeof(sf_header)+sizeof(sf_footer)))+M));
+        // prologue footer = same as header
+        nbp->prev_footer = bp->header;
+        nbp->header = (PAGE_SZ-((M-(sizeof(sf_header)+sizeof(sf_footer)))+M)-(2*sizeof(sf_header))) & BLOCK_SIZE_MASK; // set block_size to 64
+        //96
+        nbp->header |= PREV_BLOCK_ALLOCATED; //prev_alloc = 1
+
+        // create epilogue
+        bp = (sf_block *)(sf_mem_end()-(sizeof(sf_header)+sizeof(sf_footer)));
+        //bp->header = 0 & BLOCK_SIZE_MASK; // set block_size to 0
+        bp->header |= THIS_BLOCK_ALLOCATED; // alloc = 1
+        // empty/free: prev_allo = 0
+        bp->prev_footer = nbp->header;
+
+
+
+        // free lists
+
+
+    }
+    //extend heap
+
+    sf_show_heap();
+    printf("%s\n", "end malloc");
     return NULL;
 }
 
