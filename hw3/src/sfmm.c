0049feafed04ff18f33f6034c2ff808d1a532e5f
Andrea Lee <andrlee@cs.stonybrook.edu>
2020-03-22 02:56:34 -0400

    initialized free lists

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -18,42 +18,62 @@ void *sf_malloc(size_t size) {
 
     // else size !=0
 
+
+
+
     // if heap is empty
     if(sf_mem_start()==sf_mem_end()) { // initial call
+
+        //initialize free lists: list is empty
+        for(int i=0; i<NUM_FREE_LISTS; i++) {
+            sf_free_list_heads[i].body.links.next = &sf_free_list_heads[i];
+            sf_free_list_heads[i].body.links.prev = &sf_free_list_heads[i];
+        }
+
         sf_mem_grow();
-        // create prologue
-        sf_block *bp = (sf_block *)(sf_mem_start()+(M-(sizeof(sf_header)+sizeof(sf_footer))));
-        //48
-        // header = [al: 1, sz:      64, pal: 1][PROLOGUE]
+        // create prologue: header = [al: 1, sz:      64, pal: 1][PROLOGUE]
+        sf_block *bp = (sf_mem_start()+(M-(sizeof(sf_header)+sizeof(sf_footer)))); //48
         bp->header = M & BLOCK_SIZE_MASK; // set block_size to 64
         bp->header |= THIS_BLOCK_ALLOCATED; // alloc = 1
         bp->header |= PREV_BLOCK_ALLOCATED; //prev_alloc = 1
 
-        // next block in the heap = free/alloc blocks
-        sf_block *nbp = (sf_block *)(sf_mem_start()+((M-(sizeof(sf_header)+sizeof(sf_footer)))+M));
-        // prologue footer = same as header
-        nbp->prev_footer = bp->header;
-        nbp->header = (PAGE_SZ-((M-(sizeof(sf_header)+sizeof(sf_footer)))+M)-(2*sizeof(sf_header))) & BLOCK_SIZE_MASK; // set block_size to 64
-        //96
-        nbp->header |= PREV_BLOCK_ALLOCATED; //prev_alloc = 1
+        // wilderness block
+        sf_block *wbp = (sf_block *)(sf_mem_start()+((M-(sizeof(sf_header)+sizeof(sf_footer)))+M)); //112
+        wbp->prev_footer = bp->header; // prologue footer = same as header
+        wbp->header = (PAGE_SZ-((M-(sizeof(sf_header)+sizeof(sf_footer)))+M)-(2*sizeof(sf_header))) & BLOCK_SIZE_MASK; // set block_size to 3968
+        //prologue header, epilogue header
+        wbp->header |= PREV_BLOCK_ALLOCATED; //prev_alloc = 1
+        //wbp->body.links.next = (sf_block *)(sf_free_list_heads[NUM_FREE_LISTS].body.links.next);
+        printf("%s\n", "HULLO");
+        printf("%p\n", (sf_block *)(sf_free_list_heads[NUM_FREE_LISTS].body.links.next));
 
         // create epilogue
         bp = (sf_block *)(sf_mem_end()-(sizeof(sf_header)+sizeof(sf_footer)));
         //bp->header = 0 & BLOCK_SIZE_MASK; // set block_size to 0
         bp->header |= THIS_BLOCK_ALLOCATED; // alloc = 1
         // empty/free: prev_allo = 0
-        bp->prev_footer = nbp->header;
+        bp->prev_footer = wbp->header;
 
 
-
-        // free lists
 
 
     }
+    // if free lists is empty
+    for(int i=0; i<NUM_FREE_LISTS; i++) {
+        if((sf_free_list_heads[i].body.links.next != &sf_free_list_heads[i]) && (sf_free_list_heads[i].body.links.prev != &sf_free_list_heads[i])){
+            //sf_free_list_heads[i].body.links.next = ; // points to first node in the list
+            //sf_free_list_heads[i].body.links.prev = ; // points to last node in the list
+        }
+        //non empty
+    }
+
+
     //extend heap
 
+
+
+
     sf_show_heap();
-    printf("%s\n", "end malloc");
     return NULL;
 }
 
