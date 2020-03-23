40478151dde10342359cd7e3416addb85ce378b4
Andrea Lee <andrlee@cs.stonybrook.edu>
2020-03-23 16:45:47 -0400

    changed code to be modular, added more helper methods

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -26,54 +26,91 @@ void insert_free_list(void * free_list, sf_block *block) {
     block->body.links.prev = prev_block;
     block->body.links.next->body.links.prev = block;
 }
-void remove_free_list(void * free_list, sf_block *block) {
-    sf_block *prev_block = (sf_block *)free_list;
-    block->body.links.next = prev_block;
-    prev_block->body.links.next = block;
-    block->body.links.prev = prev_block;
-    block->body.links.next->body.links.prev = block;
+void delete_free_list(void * free_list, sf_block *block) {
+    block->body.links.next->body.links.prev = block->body.links.prev; //b->next->prev = b->prev
+    block->body.links.prev->body.links.next = block->body.links.next; //b->prev->next = b->next
 }
 sf_block *get_next_block(sf_block *block) {
     return (sf_block *)((char *)(block)+((block->header)&BLOCK_SIZE_MASK));
 }
+long int get_block_size(sf_block *block){
+    return block->header & BLOCK_SIZE_MASK;
+}
+void set_block_size(sf_block *block, size_t size){
+    block->header = size & BLOCK_SIZE_MASK;
+}
+long int get_alloc_bit(sf_block *block) {
+    return block->header & THIS_BLOCK_ALLOCATED;
+}
+long int get_prev_alloc_bit(sf_block *block) {
+    return block->header & PREV_BLOCK_ALLOCATED;
+}
+sf_header create_header(size_t size, long int prev_alloc, long int alloc) {
+    sf_header header = ((size&BLOCK_SIZE_MASK)|(prev_alloc)|(alloc));
+    return header;
+}
+sf_block *place_block(void * address, sf_header header) {
+    sf_block *block = (sf_block*)(address);
+    block->header = header;
+    if(get_alloc_bit(block)) {
+        //insert_free_list(block);
+    }
+    return block;
+}
+//sf_block *get_free_list(sf_block *block) {
+    //size
+//}
+sf_block *split_block(sf_block *block, size_t size) {
+    //split without creating a splinter (<=64 bytes)
+    // if split free blocksize <64, leave
+        // datasize = entire.
+        // insert
+
+    //upper part = remainder [al: 0, sz:       get_block_size(block) - size, pal: 1]
+    sf_block *free_block = place_block((char *)(block) + size, create_header(get_block_size(block) - size, PREV_BLOCK_ALLOCATED, 0));
+
+    //lower part = allocation request [al: 1, sz:       size, pal: block.pal]
+    sf_block *data_block = place_block((char *)(block), create_header(size, get_prev_alloc_bit(block), THIS_BLOCK_ALLOCATED));
+    data_block->prev_footer= block->prev_footer;
+
+    free_block->prev_footer = data_block->header;
+    get_next_block(free_block)->prev_footer = free_block->header;
+    //insert remainer back into its size class
+    delete_free_list(&sf_free_list_heads[NUM_FREE_LISTS-1], data_block);//remove allocated block from free list
+    insert_free_list(&sf_free_list_heads[NUM_FREE_LISTS-2], free_block);
+    return data_block;
+}
+
 
 void *sf_malloc(size_t size) {
-    if(size==0) {
+
+    if(size == 0) {
         return NULL;
     } // else size !=0
-
 
     // if heap is empty, initialize heap
     if(sf_mem_start()==sf_mem_end()) { // initial call
         //initialize free lists: list is empty
         initialize_free_lists();
 
-        sf_block *ptr = sf_mem_grow();
+        sf_block *ptr = sf_mem_grow(); ////////////////////////////////////
+
         if(ptr == NULL) { // no more space to allocate
             sf_errno = ENOMEM;
             return NULL;
         }
-        // create prologue. no access to prev_footer
-        sf_block *prologue = (sf_block *)(sf_mem_start()+(M-(sizeof(sf_header)+sizeof(sf_footer)))); //48
-        prologue->header = M & BLOCK_SIZE_MASK; // set block_size to 64
-        prologue->header |= THIS_BLOCK_ALLOCATED; // alloc = 1
-        prologue->header |= PREV_BLOCK_ALLOCATED; //prev_alloc = 1
+        // create prologue. (does not have prev_footer) [al: 1, sz:       64, pal: 1]
+        sf_block *prologue = place_block(sf_mem_start()+(M-(sizeof(sf_header)+sizeof(sf_footer))), create_header(M, PREV_BLOCK_ALLOCATED, THIS_BLOCK_ALLOCATED));
 
-        // wilderness block
-        sf_block *wbp = (sf_block *)((char *)(prologue)+M); //112
-        wbp->prev_footer = prologue->header; // prologue footer = same as header
-        wbp->header = (PAGE_SZ-((M-(sizeof(sf_header)+sizeof(sf_footer)))+M)-(2*sizeof(sf_header))) & BLOCK_SIZE_MASK; // set block_size to 3968 //prologue header, epilogue header
-        wbp->header &= ~(THIS_BLOCK_ALLOCATED); //alloc = 0
-        wbp->header |= PREV_BLOCK_ALLOCATED; //prev_alloc = 1
-        // insert into free list
-        insert_free_list(&sf_free_list_heads[NUM_FREE_LISTS-1], wbp);
+        // wilderness block 112 [al: 0, sz:       3968, pal: 1]
+        sf_block *wilderness = place_block((char *)(prologue)+M, create_header(PAGE_SZ-((M-(sizeof(sf_header)+sizeof(sf_footer)))+M)-(2*sizeof(sf_header)), PREV_BLOCK_ALLOCATED, 0));
+        wilderness->prev_footer = prologue->header; // prologue footer = same as header
+        // insert into free list ////////////////////////////////////
+        insert_free_list(&sf_free_list_heads[NUM_FREE_LISTS-1], wilderness);
 
-        // create epilogue. only need header, prev_footer
-        sf_block *epilogue = (sf_block *)(sf_mem_end()-(sizeof(sf_header)+sizeof(sf_footer)));
-        epilogue->header = 0 & BLOCK_SIZE_MASK; // set block size to 0
-        epilogue->header |= THIS_BLOCK_ALLOCATED; // alloc = 1
-        epilogue->header &= ~(PREV_BLOCK_ALLOCATED); //prev_alloc = 0
-        epilogue->prev_footer = wbp->header;
+        // create epilogue. (only need header, prev_footer) [al: 1, sz:       0, pal: 0]
+        sf_block *epilogue = place_block(sf_mem_end()-(sizeof(sf_header)+sizeof(sf_footer)), create_header(0, 0, THIS_BLOCK_ALLOCATED));
+        epilogue->prev_footer = wilderness->header;
     }
 
     //determine size of block to be allocated
@@ -82,38 +119,19 @@ void *sf_malloc(size_t size) {
     block_size += padding;
 
     //search for smallest free list to satisfy request size->end-2
-    //fib range
-    //(block_size/M)-1
+    /////////////////////////////////////
     for(int i = 0; i<NUM_FREE_LISTS; i++) {
         sf_block *found_block = ((sf_block *)(&sf_free_list_heads[i]))->body.links.next; //head = sentinel
         while(found_block != &sf_free_list_heads[i]) { // search link list
-            if((found_block->header & BLOCK_SIZE_MASK) == block_size) {
+            if(get_block_size(found_block) == block_size) {
                 return found_block->body.payload;
             }
-            if((found_block->header & BLOCK_SIZE_MASK) > block_size) { // block found
-                //split without creating a splinter (<=64 bytes)
-
-                //split : upper part = remainder
-                sf_block *free_block = (sf_block *)((char *)(found_block)+block_size);
-                free_block->header = (((found_block->header)&BLOCK_SIZE_MASK) - block_size) & BLOCK_SIZE_MASK;
-                free_block->header |= PREV_BLOCK_ALLOCATED; //prev_alloc = 1
-                free_block->header &= ~(THIS_BLOCK_ALLOCATED); //alloc = 0
-
-                //split : lower part = allocation request
-                sf_block *data_block = (sf_block *)(char *)(found_block);
-                data_block->header = block_size & BLOCK_SIZE_MASK;
-                //data_block->header |= THIS_BLOCK_ALLOCATED; //alloc = 1
-                data_block->header |= (found_block->header|PREV_BLOCK_ALLOCATED); //prev_alloc
-                data_block->prev_footer= found_block->prev_footer;
-
-                free_block->prev_footer = data_block->header;
-                get_next_block(free_block)->prev_footer = free_block->header;
-                //insert remainer back into its size class
-                remove_free_list(&sf_free_list_heads[NUM_FREE_LISTS-1], data_block);//remove allocated block from free list
-                insert_free_list(&sf_free_list_heads[NUM_FREE_LISTS-2], free_block);
-
-                //success return pointer to of request to payload
+            if(get_block_size(found_block) > block_size) { // block found
+                split_block(found_block, block_size);
+                ///////////////////////////////////////////////////////
+                //success return pointer to of request to payload/valid regin of memory
                 //return data_block->body.payload;
+
             }
 
             found_block = found_block->body.links.next; // move to next block in the free list
