c11f404ef9deaa9bc2c0bdb6b19251bb39905e4a
Andrea Lee <andrlee@cs.stonybrook.edu>
2020-03-23 19:47:21 -0400

    return payload address for malloc. somewhat pruned search for finding free block in malloc

--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -50,9 +50,8 @@ sf_header create_header(size_t size, long int prev_alloc, long int alloc) {
     sf_header header = ((size&BLOCK_SIZE_MASK)|(prev_alloc)|(alloc));
     return header;
 }
-sf_block *get_free_list(sf_block *block) {
+sf_block *get_free_list(long int size) {
     int fib[NUM_FREE_LISTS] = {0, M, 2*M, 3*M, 5*M, 8*M, 13*M, 21*M, 34*M, INT_MAX};
-    long int size = get_block_size(block);
     for(int index=0; index<NUM_FREE_LISTS-1; index++) {
         if(size>fib[index] && size<=fib[index+1]) {
             return &sf_free_list_heads[index+1];
@@ -64,21 +63,21 @@ sf_block *place_block(void * address, sf_header header) {
     sf_block *block = (sf_block*)(address);
     block->header = header;
     if(get_alloc_bit(block) == 0) { // add to free list
-        insert_free_list(get_free_list(block), block);
+        insert_free_list(get_free_list(get_block_size(block)), block);
     }
     return block;
 }
 sf_block *split_block(sf_block *block, size_t size) {
     long int free_block_size = get_block_size(block) - size;
+
+    //remove allocated block from free list
+    delete_free_list(get_free_list(get_block_size(block)), block);
+
     //split without creating a splinter (<=64 bytes) : 'over alloc'
-    if(free_block_size<M) { // splinter (splinter size = < min size)
+    if(free_block_size < M) { // splinter (splinter size = < min size)
         block->header |= THIS_BLOCK_ALLOCATED; // switch to alloc
-        //delete from free list
-        delete_free_list(&sf_free_list_heads[NUM_FREE_LISTS-1], block);
         return block;
     }
-    //remove allocated block from free list
-    delete_free_list(get_free_list(block), block);
 
     //upper part = remainder [al: 0, sz:       get_block_size(block) - size, pal: 1]
     sf_block *free_block = place_block((char *)(block) + size, create_header(get_block_size(block) - size, PREV_BLOCK_ALLOCATED, 0));
@@ -90,15 +89,11 @@ sf_block *split_block(sf_block *block, size_t size) {
     free_block->prev_footer = data_block->header;
     get_next_block(free_block)->prev_footer = free_block->header;
 
-    //insert remainer back into its size class
-    //insert_free_list(get_free_list(free_block), free_block);
-
     return data_block;
 }
 
 
 void *sf_malloc(size_t size) {
-
     if(size == 0) {
         return NULL;
     } // else size !=0
@@ -120,8 +115,6 @@ void *sf_malloc(size_t size) {
         // wilderness block 112 [al: 0, sz:       3968, pal: 1]
         sf_block *wilderness = place_block((char *)(prologue)+M, create_header(PAGE_SZ-((M-(sizeof(sf_header)+sizeof(sf_footer)))+M)-(2*sizeof(sf_header)), PREV_BLOCK_ALLOCATED, 0));
         wilderness->prev_footer = prologue->header; // prologue footer = same as header
-        // insert into free list ////////////////////////////////////
-        //insert_free_list(&sf_free_list_heads[NUM_FREE_LISTS-1], wilderness);
 
         // create epilogue. (only need header, prev_footer) [al: 1, sz:       0, pal: 0]
         sf_block *epilogue = place_block(sf_mem_end()-(sizeof(sf_header)+sizeof(sf_footer)), create_header(0, 0, THIS_BLOCK_ALLOCATED));
@@ -134,24 +127,25 @@ void *sf_malloc(size_t size) {
     block_size += padding;
 
     //search for smallest free list to satisfy request size->end-2
-    /////////////////////////////////////
-    for(int i = 0; i<NUM_FREE_LISTS; i++) {
-        sf_block *found_block = ((sf_block *)(&sf_free_list_heads[i]))->body.links.next; //head = sentinel
-        while(found_block != &sf_free_list_heads[i]) { // search link list
+    sf_block *free_list = get_free_list(block_size);
+    while(free_list != &sf_free_list_heads[NUM_FREE_LISTS]) {
+        sf_block *found_block = ((sf_block *)(free_list))->body.links.next; //head = sentinel
+        while(found_block != free_list) { // search link list
             if(get_block_size(found_block) == block_size) {
                 return found_block->body.payload;
             }
             if(get_block_size(found_block) > block_size) { // block found
-                split_block(found_block, block_size);
-                ///////////////////////////////////////////////////////
                 //success return pointer to of request to payload/valid regin of memory
-                //return split_block(found_block, block_size)->body.payload;
-
+                return split_block(found_block, block_size)->body.payload;
             }
-
-            found_block = found_block->body.links.next; // move to next block in the free list
+            // move to next block in the free list
+            found_block = found_block->body.links.next;
         } //if no block found, continue to next size class
+        // move to the next free list
+        free_list += 1;
     }
+
+
 
     //if block not found when end is reached- use wilderness block.
         //if wilderness block too small/does not exist<<<<<<<
